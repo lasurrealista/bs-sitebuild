3337593e9e07c7fc4f8ab67a3c9a1931d774a020
lasurrealista <ladyblue@freemail.hu>
2021-01-10 17:19:02 +0100

    Menu + responsive design

--- main.js
+++ main.js
@@ -3,3 +3,18 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+window.onscroll = function() {
+    stickMenu()
+};
+
+const navbar = document.querySelector('.navbar');
+let sticky = navbar.offsetTop;
+
+function stickMenu() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
